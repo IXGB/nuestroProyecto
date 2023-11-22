fca142117c4cf21de12ae85f5a513c6a2a1b3aaf
Owner <Owner@DESKTOP-R6JHS8I>
2023-11-22 11:26:14 -0400

    atributos paciente

--- ProyectoClinica/src/logico/Paciente.java
+++ ProyectoClinica/src/logico/Paciente.java
@@ -2,8 +2,19 @@ package logico;
 
 public class Paciente extends Persona {
 
-	public Paciente(String id, String cedula, String nombre, String telefono, String direccion, String sexo) {
+	private String NHC;
+	private HistorialClinico historial;
+	private String tipoSangre;
+	private float peso;
+	private float estatura;
+	
+	public Paciente(String id, String cedula, String nombre, String telefono, String direccion, String sexo, String NHC, HistorialClinico historial, String tipoSangre, float peso, float estatura) {
 		super(id, cedula, nombre, telefono, direccion, sexo);
+		this.NHC = NHC;
+		this.historial = historial;
+		this.tipoSangre = tipoSangre;
+		this.peso = peso;
+		this.estatura = estatura;
 		
 	}
 
