1ffce3440c0e35c184b79fd48a728f774b13f607
Owner <Owner@DESKTOP-R6JHS8I>
2023-12-01 10:47:08 -0400

    diseño princpal visual

--- ProyectoClinica/src/visual/PrincipalVisual.java
+++ ProyectoClinica/src/visual/PrincipalVisual.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class PrincipalVisual extends JFrame {
 
@@ -36,7 +39,40 @@ public class PrincipalVisual extends JFrame {
 	 */
 	public PrincipalVisual() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 764, 502);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu_2 = new JMenu("Historial Cl\u00EDnico");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Mi Historial");
+		mnNewMenu_2.add(mntmNewMenuItem_4);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Historial Paciente");
+		mnNewMenu_2.add(mntmNewMenuItem_3);
+		
+		JMenu mnNewMenu = new JMenu("Cita");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem_6 = new JMenuItem("Registrar Cita");
+		mnNewMenu.add(mntmNewMenuItem_6);
+		
+		JMenu mnNewMenu_1 = new JMenu("Administraci\u00F3n");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Listar Pacientes");
+		mnNewMenu_1.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Listar Emleados");
+		mnNewMenu_1.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_5 = new JMenuItem("Historial Cl\u00EDnica");
+		mnNewMenu_1.add(mntmNewMenuItem_5);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Administrar Viviendas");
+		mnNewMenu_1.add(mntmNewMenuItem_2);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
